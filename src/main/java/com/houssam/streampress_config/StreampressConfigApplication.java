94ca0ee84b300101d7d29207bfe8b1c6e4ba41be
houssam <lambarahoussam@gmail.com>
2026-03-09 11:12:01 +0000

    Initial Config Server project

---
+++ src/main/java/com/houssam/streampress_config/StreampressConfigApplication.java
@@ -0,0 +1,13 @@
+package com.houssam.streampress_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreampressConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreampressConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/houssam/streampress_config/StreampressConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houssam.streampress_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreampressConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
