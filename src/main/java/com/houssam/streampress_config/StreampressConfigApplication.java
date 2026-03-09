bb514c2a2f63b4d1489f3e86a015d0a9d95aab4e
houssam <lambarahoussam@gmail.com>
2026-03-09 12:02:57 +0000

    feat: add services configuration files

--- src/main/java/com/houssam/streampress_config/StreampressConfigApplication.java
+++ src/main/java/com/houssam/streampress_config/StreampressConfigApplication.java
@@ -1,13 +1,11 @@
 package com.houssam.streampress_config;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
+@EnableConfigServer
 public class StreampressConfigApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StreampressConfigApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StreampressConfigApplication.class, args);
+    }
 }
